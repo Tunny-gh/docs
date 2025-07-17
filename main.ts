5264bf175d3ead564e0840f3c251a0150b163182
hrntsm <contact@hrntsm.com>
2025-07-18 08:07:41 +0900

    Add fresh config

--- main.ts
+++ main.ts
@@ -9,5 +9,6 @@ import twindPlugin from "$fresh/plugins/twind.ts";
 
 import manifest from "./fresh.gen.ts";
 import twindConfig from "./twind.config.ts";
+import config from "./fresh.config.ts";
 
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
+await start(manifest, { ...config, plugins: [twindPlugin(twindConfig)] });
